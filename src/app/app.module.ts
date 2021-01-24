f091a474f9a8e0cb52b6cc120fb9918b8dd93e62
Marjona Mirzoeva <mirzoeva.top@mail.ru>
2021-01-24 20:19:25 +0400

    6.2. Angular (create components)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { MenuComponent } from './menu/menu.component';
+import { ProductCardComponent } from './product-card/product-card.component';
+import { FooterComponent } from './footer/footer.component';
+import { CardListComponent } from './card-list/card-list.component';
+import { BasketComponent } from './basket/basket.component';
+import { BasketRowComponent } from './basket-row/basket-row.component';
+import { CounterComponent } from './counter/counter.component';
+import { LoaderComponent } from './loader/loader.component';
+import { NotificationComponent } from './notification/notification.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    MenuComponent,
+    ProductCardComponent,
+    FooterComponent,
+    CardListComponent,
+    BasketComponent,
+    BasketRowComponent,
+    CounterComponent,
+    LoaderComponent,
+    NotificationComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/basket-row/basket-row.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-basket-row',
+  templateUrl: './basket-row.component.html',
+  styleUrls: ['./basket-row.component.less']
+})
+export class BasketRowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
